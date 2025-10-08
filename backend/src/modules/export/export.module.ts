d444da6d943089833642d8d86963ad967d73798f
Lilieth Ramirez <liliethramirez91@gmail.com>
2025-10-08 07:58:12 -0300

    export: update ExportModule

--- backend/src/modules/export/export.module.ts
+++ backend/src/modules/export/export.module.ts
@@ -3,9 +3,10 @@ import { ExportService } from './export.service';
 import { ExportController } from './export.controller';
 import { CardModule } from '../card/card.module';
 import { ColumnModule } from '../column/column.module';
+import { N8nModule } from '../n8n/n8n.module';
 
 @Module({
-  imports: [CardModule, ColumnModule],
+  imports: [CardModule, ColumnModule, N8nModule],
   controllers: [ExportController],
   providers: [ExportService],
   exports: [ExportService],
